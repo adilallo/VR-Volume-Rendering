6b5bfb4686367e5f6b5afbecfd738c82879ed63d
adilallo <39313955+adilallo@users.noreply.github.com>
2023-06-29 09:27:23 -0600

    delete

--- scripts/ci/activate-license.sh
+++
@@ -1,6 +0,0 @@
-apt install coreutils
-echo "$UNITY_LICENSE" | base64 -d > unity_license.ulf
-ls
-ls /opt/unity/Editor/
-echo "Activating unity license..."
-/opt/unity/Editor/Unity -batchmode -nographics -quit -manualLicenseFile unity_license.ulf || true
