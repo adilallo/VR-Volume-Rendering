98c85c0eeb444b77b1795fc947d12bcc81837abe
Andy DiLallo <39313955+adilallo@users.noreply.github.com>
2023-07-25 12:04:32 -0400

    adjust camera and rendered object orientation

--- Assets/Scripts/DICOM Loader/DICOMLoader.cs
+++ Assets/Scripts/DICOM Loader/DICOMLoader.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7f6078478607a6b8d99adf845ef5d96da7f9fd32bf712ed26131b237471f084
-size 828
+oid sha256:cde8274f42696ba272e2ecb0fbf5106464cf174c645a766394afb7ab69b971cc
+size 975
